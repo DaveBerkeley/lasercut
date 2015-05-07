5196f516902d07362b6a553657b86c1be8a1df5d
Dave Berkeley <dave@rotwang.co.uk>
2015-05-07 10:39:41 +0100

    added example from cartertools.com

--- gears.py
+++ gears.py
@@ -38,37 +38,64 @@ work = Collection()
 # Involute gears, see :
 # http://www.cartertools.com/involute.html
 
-nteeth = 20
-r1 = 100
-angle = 2.86
+P = 16.0
+N = 20
+PA = 14.5
+
+D = N / P
+R = D / 2.0
+DB = D * math.cos(radians(PA))
+RB = DB / 2.0
+a = 1.0 / P
+d = 1.157 / P
+DO = D + (2 * a)
+RO = DO / 2.0
+DR = D - (2 * d)
+RR = DR / 2.0
+
+CB = math.pi * DB
+fcb = RB / 20.0
+ncb = CB / fcb
+acb = 360 / ncb
+gt = 360.0 / N
+
+print D, R, DB, RB
+print a, d
+print DO, RO, DR, RR
+print CB,
+print fcb, ncb, acb, gt
+
+def scale(a):
+    return a * 254
 
 v = Polygon()
-v.add(0, 0)
+v.add(0, scale(RR))
 
-for i in range(15):
-    x, y = i * r1 / 20.0, r1
-    x, y = rotate_2d(radians(i * angle), x, y)
+# note : the range 5 .. 12 approximates to the intersection
+# with the D and DO circles
+for i in range(5, 12):
+    x, y = i * RB / 20.0, RB
+    x, y = [ scale(z) for z in [ x, y ] ]
+    x, y = rotate_2d(radians(i * acb), x, y)
     v.add(x, y)
 
-for i in range(nteeth):
+v.rotate(-gt / 4.0)
+c = Collection()
+c.add(v.copy())
+v.reflect_v()
+c.add(v.copy())
+v = c
 
+for i in range(N):
     c = v.copy()
-    a = 360 / nteeth
-    c.rotate(a * i)
+    c.rotate(gt * i)
     work.add(c)
 
-    #c = v.copy()
-    #c.reflect_v()
-    #c.rotate(a * (i + 0.75))
-    #work.add(c)
+for r in [ R, RO, RR, RB ]:
+    print r
+    c = Circle((0, 0), scale(r))
+    work.add(c)
 
-#work.add(v)
-#c = v.copy()
-#c.rotate(18)
-#work.add(c)
-
-#c = Circle((0, 0), r1)
-#work.add(c)
 commit(work)
 
 drawing.save()
