3b9c9eb230fcd5f0f45489786a00f4532c4cbfc4
Dave Berkeley <dave@rotwang.co.uk>
2015-05-08 10:36:19 +0100

    kerf comp for all points

--- kerf.py
+++ kerf.py
@@ -11,9 +11,9 @@ from dxfwrite import DXFEngine as dxf
 #
 #   Compensate for kerf
 
-def parallel(xy0, xy1, kerf, inner):
-    x0, y0 = xy0
-    x1, y1 = xy1
+def parallel(points, kerf, inner):
+    x0, y0 = points[0]
+    x1, y1 = points[1]
     dx, dy = x1 - x0, y1 - y0
     a = angle(dx, dy)
     # vector at 90 degrees to line
@@ -30,17 +30,35 @@ def eol(xy0, xy1):
     m = (y1 - y0) / (x1 - x0)
     return m, y0 - (m * x0) 
 
+def intersect(e0, e1):
+    m0, c0 = e0
+    m1, c1 = e1
+    x = (c0 - c1) / (m1 - m0)
+    y = (m0 * x) + c0
+    return x, y
+
 def dekerf(poly, kerf, inner):
     # assert Polygon
-    work = Collection()
-    for i in range(len(poly.points)-1):
-        xy0, xy1 = parallel(poly.points[i], poly.points[i+1], kerf, inner)
-        print xy0, xy1
-        #print eol(xy0, xy1)
-        p = Polygon(colour=Config.draw_colour)
-        p.add(*xy0)
-        p.add(*xy1)
-        work.add(p)
+    work = Polygon(colour=8)
+
+    def line_pairs(points):
+        first = None
+        while len(points) >= 3:
+            l0 = points[:2]
+            l1 = points[1:3]
+            yield l0, l1
+            points = points[1:]
+            if first is None:
+                first = l0
+        yield points, first
+
+    for xy0, xy1 in line_pairs(poly.points[:]):
+        xy0 = parallel(xy0, kerf, inner)
+        xy1 = parallel(xy1, kerf, inner)
+        e0, e1 = eol(*xy0), eol(*xy1)
+        x, y = intersect(e0, e1)
+        work.add(x, y)
+    work.close()
     return work
 
 #
@@ -61,9 +79,10 @@ if __name__ == "__main__":
 
     #work = make_involute(pitch_dia, N, PA)
     work = Rectangle((0, 0), (10, 10))
+    work.rotate(15)
     commit(work)
 
-    kerf = 1 # 0.2
+    kerf = 0.5
     work = dekerf(work.copy(), kerf, False)
     commit(work)
 
