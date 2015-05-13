4780fa732bb344907692d218d9a6e1fc55c1870f
Dave Berkeley <dave@rotwang.co.uk>
2015-05-13 10:04:35 +0100

    wip - layers

--- layers.py
+++ layers.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 
+import sys
+
 from render import DXF as dxf
-from laser import Config, Collection, Polygon, Circle
+from laser import Config, Collection, Polygon, Circle, Rectangle
 
 #
 #
 
-def edge():
+def edge(w, h, s, hole):
     work = Polygon()
     work.add(0, 0)
     work.add(w, 0)
@@ -15,16 +17,36 @@ def edge():
     work.add(s, h-s)
     work.add(0, h-s)
     work.close()
-    c = Circle((s/2, s/2), hole/2)
-    work.add_arc(c)
-    c = Circle((w-(s/2), s/2), hole/2)
-    work.add_arc(c)
+    # add the bolt holes
+    e = s / 2
+    locs = [
+        (e, e), (w-e, e),
+    ]
+    for x, y in locs:
+        c = Circle((x, y), hole/2)
+        work.add_arc(c)
+    return work
+
+def end_plate(w, h, s, hole):
+    work = Rectangle((0, 0), (w, h))
+    e = s / 2
+    locs = [
+        (e, e), (e, h-e), (w-e, h-e), (w-e, e),
+    ]
+    for x, y in locs:
+        c = Circle((x, y), hole/2)
+        work.add_arc(c)
     return work
 
 #
 #
 
 if __name__ == "__main__":
+
+    single = False
+    if len(sys.argv) >= 2:
+        single = True
+
     config = Config()
     drawing = dxf.drawing()
 
@@ -32,28 +54,32 @@ if __name__ == "__main__":
     h = 50.0
     s = 6.0
     hole = 3.0
+    spacing = 1.0
 
-    if 0:
+    if single:
         work = Collection()
 
-        p = edge()
+        p = edge(w, h, s, hole)
         work.add(p)
 
-        p = edge()
+        p = p.copy()
         p.rotate(180)
         p.translate(w, h)
         work.add(p)
 
+        p = end_plate(w, h, s, hole)
+        p.translate(w + spacing, 0)
+        work.add(p)
+
         work.draw(drawing, config.cut())
     else:
-        spacing = 1.0
         dx = abs(complex(s + spacing, s + spacing))
         for i in range(20):
-            work = edge()
+            work = edge(w, h, s, hole)
             work.rotate(-45)
             work.translate(i * dx, 0)
             work.draw(drawing, config.cut())
-        
+
     drawing.save()
 
 # FIN
