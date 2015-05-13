b628f01a34d1d09dbc667c327d8d7800b323550c
Dave Berkeley <dave@rotwang.co.uk>
2015-05-13 23:56:13 +0100

    add end plates to layers

--- layers.py
+++ layers.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 
 import sys
+import math
 
 from render import DXF as dxf
-from laser import Config, Collection, Polygon, Circle, Rectangle, corner
+from laser import Config, Collection, Polygon, Circle, Rectangle, corner, radians
 
 #
 #
@@ -65,7 +66,7 @@ if __name__ == "__main__":
 
     w = 40.0
     h = 50.0
-    s = 5.0
+    s = 6.0
     hole = 3.0
     spacing = 1.0
 
@@ -87,11 +88,19 @@ if __name__ == "__main__":
         work.draw(drawing, config.cut())
     else:
         dx = abs(complex(s + spacing, s + spacing))
-        for i in range(20):
+        for i in range(10):
             work = edge(w, h, s, hole)
             work.rotate(-45)
             work.translate(i * dx, 0)
             work.draw(drawing, config.cut())
+
+        work = end_plate(h, w, s, hole)
+        d = ((h - s) * math.sin(radians(45))) + spacing
+        work.translate(0, d)
+        work.draw(drawing, config.cut())
+
+        work.translate(h + spacing, 0)
+        work.draw(drawing, config.cut())
 
     drawing.save()
 
