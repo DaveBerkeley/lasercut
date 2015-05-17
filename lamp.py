91788dcb320761569fe337feff3d092d305a8615
Dave Berkeley <dave@rotwang.co.uk>
2015-05-17 09:18:05 +0100

    tidy

--- lamp.py
+++ lamp.py
@@ -12,16 +12,8 @@ import ops
 #
 
 def curve(x0, y0, r, a0, a1, min_d=0.2):
-    p = Polygon()
-    circ = math.pi * r
-    divs = circ / min_d
-    div = 360.0 / divs
-    angle = a0
-    while angle < a1:
-        x, y = rotate_2d(radians(angle), r, 0)
-        p.add(x + x0, y + y0)
-        angle += div
-    return p
+    arc = Arc((x0, y0), r, a0, a1)
+    return ops.arc_to_poly(arc)
 
 def curves(x0, y0, r0, r1, a0, a1):
     p = curve(x0, y0, r0, a0, a1)
@@ -37,8 +29,8 @@ config = Config()
 
 drawing = dxf.drawing("test.dxf")
 
-w = 100 
-h = 150
+w = 180 
+h = 250
 thick = 3
 edge = 8
 strip = 2
@@ -56,7 +48,7 @@ for angle in range(0, 90, a_step):
     rr.rotate(angle)
     rects.append(rr)
 
-for d in range(10, h, step):
+for d in range(step * 3, int(abs(complex(w, h))), step):
     c = curves(edge, edge, d, d + strip, 0, 91)
     rects.append(c)
 
@@ -65,28 +57,24 @@ shape = ops.Shape(rects[0])
 for r in rects[1:]:
     shape = shape.union(ops.Shape(r))
 
-
 r = Rectangle((0, 0), (w, h))
 shape = shape.intersection(ops.Shape(r))
 
 # cut the tabs
 
 for i in range(0, h, tab_len):
-    p = Polygon()
     if (i % 2):
         x0, y0, x1, y1 = w, i, w + tab_d, i + tab_len
     else:
         x0, y0, x1, y1 = 0, i, -tab_d, i+tab_len
 
-    p.add_poly(Rectangle((x0, y0), (x1, y1)))
-
-    shape = shape.union(ops.Shape(p))
+    r = Rectangle((x0, y0), (x1, y1))
+    shape = shape.union(ops.Shape(r))
 
 #
 r = Rectangle((edge, edge), (w-edge, h-edge))
 s = ops.Shape(r)
 r = Rectangle((0, 0), (w, h))
-
 
 s = s.symmetric_difference(ops.Shape(r))
 shape = shape.union(s)
