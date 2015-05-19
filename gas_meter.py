27c4c24e0f989ea085468fe051fe77593704ec3f
Dave Berkeley <dave@rotwang.co.uk>
2015-05-19 18:03:10 +0100

    add chip and sensor positioning

--- gas_meter.py
+++ gas_meter.py
@@ -15,19 +15,35 @@ h_inner = 50.1
 edge = 8.0
 hole_r = 3 / 2.0
 w_outer, h_outer = w_inner+(2*edge), h_inner+(2*edge)
+dial_r = 10.0
+dial_dy = 25.0
+#sensor_from_edge = 16.0
+dial_dx = 16.0 # from rh edge
+
+# Nano dimensions
+nano_w = 17.9
+nano_h = 43.3
+# other d
+board_edge_w = 3
+board_edge_h = 5
+# ADNS2610 chip
+chip_body = 9.1
+chip_w = 12.85
+chip_h = 9.9
+chip_r = 5.6 / 2
+chip_dy = chip_h - 4.45
+
+# PCB dimensions
+board_h = nano_h + (2 * board_edge_h)
+board_w = nano_w + chip_w + (3 * board_edge_w)
 
 # Plate dimensions
 plate_h = h_inner + (2.0 * edge)
-plate_w = 35.0
-sensor_from_edge = 16.0
-plate_dx = edge + (w_inner - sensor_from_edge) - (plate_w/2.0)
+plate_w = board_w
 
 # corner supports
 support_w = 15.0
 
-# PCB dimensions
-board_h = 35.0
-board_w = 30.0
 board_dx = (plate_w - board_w) / 2.0
 board_dy = edge + ((h_inner - board_h) / 2.0) # mid
 mouse_w = 5.0
@@ -35,8 +51,23 @@ mouse_h = 5.0
 mouse_dx = (board_w - mouse_w) / 2.0
 mouse_dy = (board_h - mouse_h) / 2.0
 led_r = 2.0
-led_dx = mouse_dx + (mouse_w / 2.0)
-led_dy = mouse_dy + mouse_h + 3
+led_dy = 7.0 # above centre of sensor
+
+#
+#
+
+def make_chip(draw):
+    work = Collection()
+    c = Circle((chip_w/2.0, chip_dy), chip_r)
+    work.add(c)
+    work.info = { "sensor" : c }
+    if draw:
+        r = Rectangle((0, 0), (chip_w, chip_h), colour=Config.draw_colour)
+        work.add(r)
+        r = Rectangle((0, 0), (chip_body, chip_h), colour=Config.draw_colour)
+        r.translate((chip_w - chip_body) / 2.0, 0)
+        work.add(r)
+    return work
 
 #
 #
@@ -60,20 +91,37 @@ def make_board(draw):
         c = Circle((x, y), hole_r)
         work.add(c)
 
-    # hole for the camera
-    r = Rectangle((0, 0), (mouse_w, mouse_h))
-    r.translate(mouse_dx, mouse_dy)
-    work.add(r)
+    if draw:
+        r = Rectangle((0, 0), (nano_w, nano_h), colour=Config.draw_colour)
+        r.translate(board_edge_w, board_edge_h)
+        work.add(r)
 
-    # hole for the LED
-    c = Circle((0, 0), led_r)
-    c.translate(led_dx, led_dy)
+    # add the chip so it's chip_dy optical centre aligns with the dial centre
+    chip = make_chip(draw)
+    dy = edge + dial_dy - board_dy - chip_dy
+    chip.translate((board_edge_w * 2) + nano_w, dy)
+    work.add(chip)
+
+    sensor = chip.info["sensor"]
+    c = Circle((sensor.x, sensor.y), chip_r * 1.5)
     work.add(c)
 
+    # hole for the LED
+    c = Circle((sensor.x, sensor.y + led_dy), led_r)
+    work.add(c)
+
+    work.info = { 
+        "sensor" : (sensor.x, sensor.y),
+        "board"  : (board_w, board_h),
+    }
     return work
+
+#
+#
 
 def make_face(draw):
     work = Collection()
+    work.info = {}
 
     inset = edge / 2.0
     r = Rectangle((0, 0), (w_outer, h_outer))
@@ -89,9 +137,16 @@ def make_face(draw):
     work = corner(work, (0, h_outer), edge)
     work = corner(work, (w_outer, h_outer), edge)
 
+    board = make_board(draw)
+    bdx, bdy = board.info["board"]
+    sdx, sdy = board.info["sensor"]
+    work.info.update(board.info)
+
     # fixing holes for the plate
     dy = edge / 2.0
-    dx = plate_dx
+    d = sdx + dial_dx # d = from board left edge to inner right edge
+    dx = edge + (w_inner - d)
+    work.info["board_dx"] = dx
     xy = [
         (dx+inset, dy), 
         (dx+plate_w-inset, dy), 
@@ -123,10 +178,19 @@ def make_face(draw):
             dx += digit_dx
             work.add(d)
 
+    # show the position of the dial
+    if draw:
+        x, y = edge + w_inner - dial_dx, edge + dial_dy
+        c = Circle((x, y), dial_r, colour=Config.draw_colour)
+        work.add(c)
+        c = Circle((x, y), 3.0, colour=Config.draw_colour)
+        work.add(c)
+
     info = {}
     info["surround"] = work
 
     # make the plate
+
     r = Rectangle((0, 0), (plate_w, plate_h))
     for p in r.points[:-1]:
         def fn(a):
@@ -145,6 +209,8 @@ def make_face(draw):
     work.add(p)
 
     info["plate"] = work
+
+    # make the corner supports
 
     work = Polygon()
     work.add(0, 0)
@@ -178,11 +244,14 @@ if __name__ == "__main__":
 
     if draw:
         work = Collection()
-        p = info["plate"]
-        p.translate(plate_dx, 0)
-        work.add(p)
 
         p = info["surround"]
+        print p.info
+        plate_dx = p.info["board_dx"]
+        work.add(p)
+
+        p = info["plate"]
+        p.translate(plate_dx, 0)
         work.add(p)
 
         p = info["corner"].copy()
