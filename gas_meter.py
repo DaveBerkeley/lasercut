f2297af708036fd54efab47ccc98a2ad92d42b71
Dave Berkeley <dave@rotwang.co.uk>
2015-05-19 10:40:32 +0100

    simple design - wrong dimensions

--- gas_meter.py
+++ gas_meter.py
@@ -1,28 +1,44 @@
 #!/usr/bin/python
 
-from laser import Rectangle, Polygon, Circle, Arc, Collection, Config
-from laser import radians, degrees, angle, rotate_2d, splice, corner
+import sys
+
+from laser import Rectangle, Polygon, Circle, Collection, Config
+from laser import corner
 from render import DXF as dxf
 
 #
 #
 
+# Surround dimensions
 w_inner = 200
 h_inner = 150
 edge = 10
 hole_r = 3 / 2.0
+w_outer, h_outer = w_inner+(2*edge), h_inner+(2*edge)
+
+# Plate dimensions
 plate_h = h_inner + (2 * edge)
 plate_w = 50
 plate_dx = 140
 
+# corner supports
+support_w = 30
+
+# PCB dimensions
 board_h = 40
 board_w = 45
 board_dx = (plate_w - board_w) / 2.0
-board_dy = 60
+board_dy = 100
 mouse_w = 5
 mouse_h = 5
 mouse_dx = (board_w - mouse_w) / 2.0
 mouse_dy = (board_h - mouse_h) / 2.0
+led_r = 2.0
+led_dx = mouse_dx + (mouse_w / 2.0)
+led_dy = mouse_dy + mouse_h + 3
+
+#
+#
 
 def make_board(draw):
     work = Collection()
@@ -31,6 +47,7 @@ def make_board(draw):
     if draw:
         work.add(r)
 
+    # fixing holes
     inset = 3.0
     for point in r.points[:-1]:
         x, y = point
@@ -42,16 +59,22 @@ def make_board(draw):
         c = Circle((x, y), hole_r)
         work.add(c)
 
+    # hole for the camera
     r = Rectangle((0, 0), (mouse_w, mouse_h))
     r.translate(mouse_dx, mouse_dy)
     work.add(r)
+
+    # hole for the LED
+    c = Circle((0, 0), led_r)
+    c.translate(led_dx, led_dy)
+    work.add(c)
 
     return work
 
 def make_face(draw):
     work = Collection()
 
-    w_outer, h_outer = w_inner+(2*edge), h_inner+(2*edge)
+    inset = edge / 2.0
     r = Rectangle((0, 0), (w_outer, h_outer))
     work.add(r)
 
@@ -59,17 +82,41 @@ def make_face(draw):
     r.translate(edge, edge)
     work.add(r)
 
+    # round the corners
     rad = 10
     work = corner(work, (0, 0), rad)
     work = corner(work, (w_outer, 0), rad)
     work = corner(work, (0, h_outer), rad)
     work = corner(work, (w_outer, h_outer), rad)
 
+    # fixing holes for the plate
+    dy = edge / 2.0
+    dx = plate_dx
+    xy = [
+        (dx+inset, dy), 
+        (dx+plate_w-inset, dy), 
+        (dx+inset, h_outer-dy), 
+        (dx+plate_w-inset, h_outer-dy),
+    ]
+    for x, y in xy:
+        c = Circle((x, y), hole_r)
+        work.add(c)
+
+    # fixing holes for the corners
+    c = Circle((support_w+(edge/2), h_outer-(edge/2)), hole_r)
+    work.add(c)
+    c = Circle((edge/2, h_outer-edge-support_w+(edge/2)), hole_r)
+    work.add(c)
+    c = Circle((support_w+edge-(edge/2), (edge/2)), hole_r)
+    work.add(c)
+    c = Circle((edge/2, support_w+edge-(edge/2)), hole_r)
+    work.add(c)
+
     info = {}
     info["surround"] = work
 
+    # make the plate
     r = Rectangle((0, 0), (plate_w, plate_h))
-    inset = edge / 2.0
     for p in r.points[:-1]:
         def fn(a):
             if a:
@@ -79,9 +126,30 @@ def make_face(draw):
         x, y = fn(x), fn(y)
         c = Circle((x, y), hole_r)
         r.add_arc(c)
-    info["plate"] = r
 
-    info["board"] = make_board(draw)
+    # add the board to the plate
+    work = Collection(r)
+    p = make_board(draw)
+    p.translate(board_dx, board_dy)
+    work.add(p)
+
+    info["plate"] = work
+
+    work = Polygon()
+    work.add(0, 0)
+    work.add(0, support_w+edge) # corner
+    work.add(support_w+edge, support_w+edge)
+    work.add(support_w+edge, support_w)
+    work.add(edge, 0)
+    work.close()
+    work = corner(work, (0, support_w+edge), edge)
+
+    c = Circle((support_w+(edge/2), support_w+(edge/2)), hole_r)
+    work.add(c)
+    c = Circle((edge/2, edge/2), hole_r)
+    work.add(c)
+
+    info["corner"] = work
 
     return info
 
@@ -90,24 +158,51 @@ def make_face(draw):
 
 if __name__ == "__main__":
     config = Config()
-    draw = True
+
+    draw = len(sys.argv) > 1
 
     drawing = dxf.drawing("test.dxf")
 
     info = make_face(draw)
 
-    work = Collection()
-    p = info["surround"]
-    work.add(p)
+    if draw:
+        work = Collection()
+        p = info["plate"]
+        p.translate(plate_dx, 0)
+        work.add(p)
 
-    p = info["plate"]
-    p.translate(plate_dx, 0)
-    work.add(p)
+        p = info["surround"]
+        work.add(p)
 
-    p = info["board"]
-    p.translate(plate_dx, 0)
-    p.translate(board_dx, board_dy)
-    work.add(p)
+        p = info["corner"].copy()
+        p.translate(0, h_outer-edge-support_w)
+        work.add(p)
+
+        p = info["corner"].copy()
+        p.rotate(90)
+        p.translate(edge+support_w, 0)
+        work.add(p)
+    else:
+        spacing = 1.0
+        work = Collection()
+        p = info["plate"]
+        p.rotate(-90)
+        dy = plate_w+edge+spacing
+        p.translate(edge+spacing, dy)
+        work.add(p)
+
+        p = info["surround"]
+        work.add(p)
+
+        p = info["corner"].copy()
+        dy += spacing
+        p.translate(edge+spacing, dy)
+        work.add(p)
+
+        p = info["corner"].copy()
+        dy += edge + support_w + spacing
+        p.translate(edge+spacing, dy)
+        work.add(p)
 
     work.draw(drawing, config.cut())
 
