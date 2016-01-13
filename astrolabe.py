d04feb6b09ab12b7630db685f605463774178b81
Dave Berkeley <git@rotwang.co.uk>
2016-01-13 20:10:46 +0000

    interestion of circles - wip

--- astrolabe.py
+++ astrolabe.py
@@ -31,6 +31,41 @@ def project(angle):
 #
 #
 
+def intersect(x0, r0, x1, r1):
+    y0, y1 = 0, 0
+    # http://paulbourke.net/geometry/circlesphere/
+    dx = x1 - x0
+    dy = y1 - y0
+
+    d = math.sqrt((dy*dy) + (dx*dx))
+
+    if d > (r0 + r1): # seperate
+        return None
+    if d < abs(r0 - r1): # nested
+        return None
+    if (d == 0) and (r0 == r1): # co-incident
+        return None
+
+    a = ((r0*r0) - (r1*r1) + (d*d)) / (2 * d)
+
+    x2 = x0 + (dx * (a/d))
+    y2 = y0 + (dy * (a/d))
+
+    h = math.sqrt((r0*r0) - (a*a))
+
+    rx = -dy * (h/d)
+    ry = dx * (h/d)
+
+    xi = x2 + rx
+    xii = x2 - rx
+    yi = y2 + ry
+    yii = y2 - ry
+
+    return xi, yi, xii, yii
+
+#
+#
+
 class Circ:
 
     def __init__(self, x, r):
@@ -39,6 +74,10 @@ class Circ:
 
     def shape(self, s):
         return Circle((self.x * s, 0), self.r * s)
+
+    def intersect(self, c):
+        return intersect(self.x, self.r, c.x, c.r)
+
 
 #
 #
@@ -52,16 +91,26 @@ if __name__ == "__main__":
 
     drawing = dxf.drawing("test.dxf")
 
+    unit = Circ(0, 1)
+    c = unit.shape(s)
+    work.add(c)
+
     for a in range(0, 91, 5):
+        print a,
         m, r = project(a)
-        print a, m, r
+        #print a, m, r
         cc = Circ(m, r)
+        ii = cc.intersect(unit)
+        if ii:
+            x0, y0, x1, y1 = ii
+            print ii
+            c = Circle((s* x0, s * y0), s * 0.05)
+            work.add(c)
+            c = Circle((s* x1, s * y1), s * 0.05)
+            work.add(c)
+
         c = cc.shape(s)
         work.add(c)
-
-    cc = Circ(0, 1)
-    c = cc.shape(s)
-    work.add(c)
 
     work.draw(drawing, config.cut())
 
