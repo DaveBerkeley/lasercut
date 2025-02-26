76156e79dc924da44f61dd1b977cfef7a9c7c5b0
Dave Berkeley <aea@rotwang.co.uk>
2025-02-26 11:30:39 +0000

    Moved astrolabe into separate repo

--- astrolabe.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-./astrolabe.py --size 155 --code scad  --hole 5.5 rete
-./astrolabe.py --size 155 --code pdf --clock --nautical --civil --astronomical --hole 5.5 mater plate
-
-#   FIN
